a36948812c4b900bd07ebb96a3060426710e7775
fynn-burger <144301106+fynn-burger@users.noreply.github.com>
2025-05-07 10:55:22 +0200

    Refactor potential calculation

--- src/model/potential_calculation.py
+++ src/model/potential_calculation.py
@@ -14,30 +14,27 @@ def calculate_potential(gc, pos, substrate, forward_on, reverse_on, ff_inter_on,
     outer_lig_patch, outer_rec_patch = create_outer_patches(gc, pos, substrate)
 
     # Initialize interaction values
-    trans_sig_fwd = trans_sig_rev = 0
-    ff_sig_fwd = ff_sig_rev = 0
-    cis_sig_fwd = cis_sig_rev = 0
     forward_sig = reverse_sig = 0
 
     # Compute interactions only if needed
     if ft_inter_on:
-        trans_sig_fwd = (outer_rec_patch * substrate.ligands).sum()
-        trans_sig_rev = (outer_lig_patch * substrate.receptors).sum()
+        trans_sig_fwd, trans_sig_rev = calculate_ft_inter(outer_lig_patch, outer_rec_patch,
+                                                          substrate.ligands, substrate.receptors)
+        forward_sig += trans_sig_fwd
+        reverse_sig += trans_sig_rev
     if ff_inter_on:
-        ff_coef = calculate_ff_coef(step, num_steps, sigmoid_steepness, sigmoid_shift, sigmoid_height)
-        other_gc_lig_patch = all_gc_lig - outer_lig_patch
-        other_gc_rec_patch = all_gc_rec - outer_rec_patch
-        ff_sig_fwd = (other_gc_lig_patch * outer_rec_patch * ff_coef).sum()
-        ff_sig_rev = (other_gc_rec_patch * outer_lig_patch * ff_coef).sum()
-    if cis_inter_on:
-        inner_lig_patch, inner_rec_patch = create_inner_patches(gc)
-        cis_sig_fwd = cis_sig_rev = (inner_lig_patch * inner_rec_patch).sum()
+        ff_sig_fwd, ff_sig_rev = calculate_ff_inter(step, num_steps, sigmoid_steepness, sigmoid_shift, sigmoid_height,
+                                                    all_gc_lig, all_gc_rec, outer_lig_patch, outer_rec_patch)
+        forward_sig += ff_sig_fwd
+        reverse_sig += ff_sig_rev
 
-    # Calculate the forward and reverse signals based on flags
-    if forward_on:
-        forward_sig = trans_sig_fwd + cis_sig_fwd + ff_sig_fwd
-    if reverse_on:
-        reverse_sig = trans_sig_rev + cis_sig_rev + ff_sig_rev
+    if cis_inter_on:
+        cis_sig = calculate_cis_inter(gc)
+        forward_sig += cis_sig
+        reverse_sig += cis_sig
+
+    forward_sig = forward_sig if forward_on else 0
+    reverse_sig = reverse_sig if reverse_on else 0
 
     # Ensure signals are strictly positive and rounded
     forward_sig = max(float(f"{forward_sig:.6f}"), 1e-4)
@@ -46,6 +43,25 @@ def calculate_potential(gc, pos, substrate, forward_on, reverse_on, ff_inter_on,
     # Calculate and return the potential
     return abs(math.log(reverse_sig) - math.log(forward_sig))
 
+
+def calculate_ft_inter(lig_patch, rec_patch, substrate_ligands, substrate_receptors):
+    trans_sig_fwd = (rec_patch * substrate_ligands).sum()
+    trans_sig_rev = (lig_patch * substrate_receptors).sum()
+    return trans_sig_fwd, trans_sig_rev
+
+def calculate_ff_inter(step, num_steps, sigmoid_steepness, sigmoid_shift, sigmoid_height, all_gc_lig, all_gc_rec,
+                       lig_patch, rec_patch):
+    ff_coef = calculate_ff_coef(step, num_steps, sigmoid_steepness, sigmoid_shift, sigmoid_height)
+    other_gc_lig_patch = all_gc_lig - lig_patch
+    other_gc_rec_patch = all_gc_rec - rec_patch
+    ff_sig_fwd = (other_gc_lig_patch * rec_patch * ff_coef).sum()
+    ff_sig_rev = (other_gc_rec_patch * lig_patch * ff_coef).sum()
+    return ff_sig_fwd, ff_sig_rev
+
+def calculate_cis_inter(gc):
+    inner_lig_patch, inner_rec_patch = create_inner_patches(gc)
+    cis_sig = (inner_lig_patch * inner_rec_patch).sum()
+    return cis_sig
 
 def calculate_ff_coef(step, num_steps, sigmoid_steepness, sigmoid_shift, sigmoid_height=1):
     """
@@ -77,12 +93,8 @@ def create_outer_patches(gc, pos, substrate):
     return lig_patch, rec_patch
 
 
-
 def create_inner_patches(gc):
     lig_patch = gc.gauss_kernel * gc.inner_ligand_current
     rec_patch = gc.gauss_kernel * gc.inner_receptor_current
 
     return lig_patch, rec_patch
-
-
-
